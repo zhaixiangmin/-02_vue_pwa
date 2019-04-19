9839c8b0d7820ab42bfc67447a702ddd5a007f40
zhaixiangmin <262832930@qq.com>
2019-04-20 03:04:42 +0800

    0304

---
+++ precache-manifest.4a157c8542e37e3b08cb3c1d6eba2c58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "01628052d22c24515876",
+    "url": "/js/chunk-vendors.09dc00ac.js"
+  },
+  {
+    "revision": "9d59ec03ac3835f71e19",
+    "url": "/js/app.33654260.js"
+  },
+  {
+    "revision": "4f6da1dbe51419fdcc6183e58ffdb133",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9d59ec03ac3835f71e19",
+    "url": "/css/app.a510f641.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,4 @@
-importScripts("/precache-manifest.e994662b17708012c4f06c72a582437a.js", "https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js");
-
-
+importScripts("/precache-manifest.4a157c8542e37e3b08cb3c1d6eba2c58.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.core.setCacheNameDetails({prefix: "02_vue_pwa"});
 
@@ -13,16 +11,16 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-
 // 动态ajax请求的缓存
-workbox.routing.registerRoute(function(obj){
-    // console.log(obj);  // {url: URL, event: FetchEvent}
-    // console.log(obj.url);
-    // 返回是否缓存结果
-    if (obj.url.pathname.includes('getlunbo')) {
-        return true;
-    } else {
-        return false;
-    }
-    // 缓存策略
-},workbox.strategies.cacheFirst());
+workbox.routing.registerRoute(function (obj) {
+  // console.log('obj', obj);
+  // console.log('obj.url', obj.url);
+  // 返回是否缓存结果
+  if (obj.url.pathname.includes('findImageByType')) {
+    return true;
+  } else {
+    return false;
+  }
+  // 缓存策略
+}, workbox.strategies.cacheFirst());
+
